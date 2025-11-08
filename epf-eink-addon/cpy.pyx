00a398b4e6c840bc38320b177ab8bb3d8cc60c35
Zippo2000 <53836590+Zippo2000@users.noreply.github.com>
2025-11-08 18:38:29 +0100

    1.0.2

--- epf-eink-addon/cpy.pyx
+++ epf-eink-addon/cpy.pyx
@@ -18,14 +18,9 @@ ctypedef np.uint8_t UINT8_TYPE
 cdef uint32_t sqDiff(uint16_t a, uint16_t b) nogil:
     return (a - b) * (a - b)
 
-#---------------------
-# Original Floyd-Steinberg
 cdef int closestColor(uint16_t r, uint16_t g, uint16_t b, double[:, :] linearPalette) nogil:
-    """
-    找出最接近的顏色索引，使用加權的平方距離計算
-    """
     cdef int color = 0
-    cdef uint32_t best = 2**32 - 1  # MaxUint32
+    cdef uint32_t best = 2**32 - 1
     cdef uint32_t dist
     cdef int i, j
     cdef double rr, gg, bb
@@ -35,12 +30,7 @@ cdef int closestColor(uint16_t r, uint16_t g, uint16_t b, double[:, :] linearPal
         gg = linearPalette[i, 1]
         bb = linearPalette[i, 2]
         
-        # 使用加權的平方距離計算
-        dist = (
-            ((1063 * sqDiff(r, <uint16_t>(rr * 255)) // 5000)) +
-            ((447 * sqDiff(g, <uint16_t>(gg * 255)) // 625)) +
-            ((361 * sqDiff(b, <uint16_t>(bb * 255)) // 5000))
-        )
+        dist = (((1063 * sqDiff(r, <uint16_t>(rr * 255)) // 5000)) + ((447 * sqDiff(g, <uint16_t>(gg * 255)) // 625)) + ((361 * sqDiff(b, <uint16_t>(bb * 255)) // 5000)))
         
         if dist < best:
             if dist == 0:
@@ -100,17 +90,146 @@ def load_scaled(image, angle, display_mode='fit'):
     return img
 
 def convert_image(input_image, preview_path=None, dithering_strength=1.0):
-    """Cython-optimized image conversion with Floyd-Steinberg dithering."""
     cdef np.ndarray[np.uint8_t, ndim=3] img_array = np.array(input_image, dtype=np.uint8)
+    cdef double[:, :] epd_colors = np.array([[0.0, 0.0, 0.0], [1.0, 1.0, 1.0], [1.0, 0.953, 0.220], [0.749, 0.0, 0.0], [0.392, 0.251, 1.0], [0.263, 0.541, 0.110]], dtype=np.float64)
+    cdef np.ndarray[np.uint8_t, ndim=3] pixels = np.zeros((EPD_H, EPD_W, 3), dtype=np.uint8)
+    cdef np.ndarray[np.uint8_t, ndim=3] output_img = np.zeros((EPD_H, EPD_W, 3), dtype=np.uint8)
     
-    # KORRIGIERTE PALETTE - Waveshare Spectra 6
-    cdef double[:, :] epd_colors = np.array([
-        [0.0, 0.0, 0.0],          # Black
-        [1.0, 1.0, 1.0],          # White
-        [1.0, 0.953, 0.220],      # Yellow (255, 243, 56)
-        [0.749, 0.0, 0.0],        # Red (191, 0, 0)
-        [0.392, 0.251, 1.0],      # Blue (100, 64, 255)
-        [0.263, 0.541, 0.110]     # Green (67, 138, 28)
-    ], dtype=np.float64)
+    cdef int x, y, c, best, i
+    cdef double diff, min_diff, scaled_diff, new_val
     
-    cdef np.ndarray[np.uint8_t, ndim=3] pixels = np.zeros((EPD_H, EPD_W, 3), dtype=
+    for y in range(EPD_H):
+        for x in range(EPD_W):
+            for c in range(3):
+                pixels[y, x, c] = img_array[y, x, c]
+    
+    for y in range(EPD_H):
+        for x in range(EPD_W):
+            min_diff = 1e10
+            best = 0
+            
+            for c in range(epd_colors.shape[0]):
+                diff = 0
+                for i in range(3):
+                    diff += (pixels[y, x, i] / 255.0 - epd_colors[c, i]) ** 2
+                
+                if diff < min_diff:
+                    min_diff = diff
+                    best = c
+            
+            for c in range(3):
+                diff = (pixels[y, x, c] / 255.0 - epd_colors[best, c])
+                scaled_diff = diff * dithering_strength
+                
+                if x+1 < EPD_W:
+                    new_val = pixels[y, x+1, c] + (scaled_diff * 7.0/16.0 * 255.0)
+                    pixels[y, x+1, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if x-1 >= 0 and y+1 < EPD_H:
+                    new_val = pixels[y+1, x-1, c] + (scaled_diff * 3.0/16.0 * 255.0)
+                    pixels[y+1, x-1, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if y+1 < EPD_H:
+                    new_val = pixels[y+1, x, c] + (scaled_diff * 5.0/16.0 * 255.0)
+                    pixels[y+1, x, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if x+1 < EPD_W and y+1 < EPD_H:
+                    new_val = pixels[y+1, x+1, c] + (scaled_diff * 1.0/16.0 * 255.0)
+                    pixels[y+1, x+1, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+            
+            for c in range(3):
+                output_img[y, x, c] = <np.uint8_t>(epd_colors[best, c] * 255.0)
+    
+    return output_img
+
+cdef int closestColor_Atkinson(int r, int g, int b, double[:, :] epd_colors) nogil:
+    cdef int color = 0
+    cdef double best = 1e10
+    cdef double dist
+    cdef int i
+    cdef double rr, gg, bb
+    
+    for i in range(epd_colors.shape[0]):
+        rr = epd_colors[i, 0]
+        gg = epd_colors[i, 1]
+        bb = epd_colors[i, 2]
+        
+        dist = (((1063 * sqDiff(<uint16_t>r, <uint16_t>(rr * 255)) // 5000)) + ((447 * sqDiff(<uint16_t>g, <uint16_t>(gg * 255)) // 625)) + ((361 * sqDiff(<uint16_t>b, <uint16_t>(bb * 255)) // 5000)))
+        
+        if dist < best:
+            if dist == 0:
+                return i
+            color = i
+            best = dist
+    
+    return color
+
+cdef double sqDiff_Atkinson(double a, double b) nogil:
+    return (a - b) * (a - b)
+
+def convert_image_atkinson(input_image, preview_path=None, dithering_strength=1.0):
+    cdef np.ndarray[np.uint8_t, ndim=3] img_array = np.array(input_image, dtype=np.uint8)
+    cdef double[:, :] epd_colors = np.array([[0.0, 0.0, 0.0], [1.0, 1.0, 1.0], [1.0, 0.953, 0.220], [0.749, 0.0, 0.0], [0.392, 0.251, 1.0], [0.263, 0.541, 0.110]], dtype=np.float64)
+    cdef np.ndarray[np.uint8_t, ndim=3] pixels = np.zeros((EPD_H, EPD_W, 3), dtype=np.uint8)
+    cdef np.ndarray[np.uint8_t, ndim=3] output_img = np.zeros((EPD_H, EPD_W, 3), dtype=np.uint8)
+    
+    cdef int x, y, c, best, i
+    cdef double diff, min_diff, scaled_diff, error_value, new_val
+    
+    for y in range(EPD_H):
+        for x in range(EPD_W):
+            for c in range(3):
+                pixels[y, x, c] = img_array[y, x, c]
+    
+    for y in range(EPD_H):
+        for x in range(EPD_W):
+            min_diff = 1e10
+            best = 0
+            
+            for c in range(epd_colors.shape[0]):
+                diff = 0.0
+                for i in range(3):
+                    diff += (pixels[y, x, i] / 255.0 - epd_colors[c, i]) ** 2
+                
+                if diff < min_diff:
+                    min_diff = diff
+                    best = c
+            
+            for c in range(3):
+                diff = (pixels[y, x, c] / 255.0 - epd_colors[best, c])
+                scaled_diff = diff * dithering_strength * 0.75
+                
+                if x+1 < EPD_W:
+                    error_value = scaled_diff * (1.0/8.0) * 255.0
+                    new_val = pixels[y, x+1, c] + error_value
+                    pixels[y, x+1, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if x+2 < EPD_W:
+                    error_value = scaled_diff * (1.0/8.0) * 255.0
+                    new_val = pixels[y, x+2, c] + error_value
+                    pixels[y, x+2, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if x-1 >= 0 and y+1 < EPD_H:
+                    error_value = scaled_diff * (1.0/8.0) * 255.0
+                    new_val = pixels[y+1, x-1, c] + error_value
+                    pixels[y+1, x-1, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if y+1 < EPD_H:
+                    error_value = scaled_diff * (1.0/8.0) * 255.0
+                    new_val = pixels[y+1, x, c] + error_value
+                    pixels[y+1, x, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if x+1 < EPD_W and y+1 < EPD_H:
+                    error_value = scaled_diff * (1.0/8.0) * 255.0
+                    new_val = pixels[y+1, x+1, c] + error_value
+                    pixels[y+1, x+1, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+                
+                if y+2 < EPD_H:
+                    error_value = scaled_diff * (1.0/8.0) * 255.0
+                    new_val = pixels[y+2, x, c] + error_value
+                    pixels[y+2, x, c] = <np.uint8_t>min(max(new_val, 0.0), 255.0)
+            
+            for c in range(3):
+                output_img[y, x, c] = <np.uint8_t>(epd_colors[best, c] * 255.0)
+    
+    return output_img
