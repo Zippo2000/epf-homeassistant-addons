f2441eb726480d6bfe0fdddd277301d05bffd76c
Zippo2000 <53836590+Zippo2000@users.noreply.github.com>
2025-11-09 12:11:37 +0100

    1.0.2

--- epf-eink-addon/run.sh
+++ epf-eink-addon/run.sh
@@ -4,13 +4,20 @@ bashio::log.info "Starting EPF E-Ink Add-on..."
 
 export IMMICH_API_KEY=$(bashio::config 'immich_api_key')
 export IMMICH_URL=$(bashio::config 'immich_url')
-export ALBUM_NAME=$(bashio::config 'album_name' 'EPF')
-export ROTATION_ANGLE=$(bashio::config 'rotation_angle' '0')
-export COLOR_ENHANCE=$(bashio::config 'color_enhance' '1.0')
-export CONTRAST=$(bashio::config 'contrast' '1.0')
-export SLEEP_DURATION=$(bashio::config 'sleep_duration' '3600')
+export ALBUM_NAME=$(bashio::config 'album_name' 'eink')
+export ROTATION_ANGLE=$(bashio::config 'rotation_angle' '270')
+export COLOR_ENHANCE=$(bashio::config 'color_enhance' '1.8')
+export CONTRAST=$(bashio::config 'contrast' '0.9')
+export DITHERING_STRENGTH=$(bashio::config 'dithering_strength' '1.0')
+export DISPLAY_MODE=$(bashio::config 'display_mode' 'fill')
+export IMAGE_ORDER=$(bashio::config 'image_order' 'random')
+export DITHERING_METHOD=$(bashio::config 'dithering_method' 'atkinson')
+export WAKEUP_INTERVAL=$(bashio::config 'wakeup_interval' '1440')
+export SLEEP_START_HOUR=$(bashio::config 'sleep_start_hour' '23')
+export SLEEP_START_MINUTE=$(bashio::config 'sleep_start_minute' '0')
+export SLEEP_END_HOUR=$(bashio::config 'sleep_end_hour' '6')
+export SLEEP_END_MINUTE=$(bashio::config 'sleep_end_minute' '0')
 export LOG_LEVEL=$(bashio::config 'log_level' 'info')
-export IMAGE_QUALITY=$(bashio::config 'image_quality' '85')
 
 # Set INGRESS_PATH directly (Home Assistant provides this automatically)
 # If running in Ingress mode, HA handles the routing without needing the token
@@ -32,10 +39,13 @@ bashio::log.info "  Album: ${ALBUM_NAME}"
 bashio::log.info "  Rotation: ${ROTATION_ANGLE}°"
 bashio::log.info "  Color Enhance: ${COLOR_ENHANCE}"
 bashio::log.info "  Contrast: ${CONTRAST}"
-bashio::log.info "  Sleep Duration: ${SLEEP_DURATION}s"
+bashio::log.info "  Dithering Strength: ${DITHERING_STRENGTH}"
+bashio::log.info "  Display Mode: ${DISPLAY_MODE}"
+bashio::log.info "  Image Order: ${IMAGE_ORDER}"
+bashio::log.info "  Dithering Method: ${DITHERING_METHOD}"
+bashio::log.info "  Wake Up Interval: ${WAKEUP_INTERVAL} minutes"
+bashio::log.info "  Sleep Time: ${SLEEP_START_HOUR}:${SLEEP_START_MINUTE} - ${SLEEP_END_HOUR}:${SLEEP_END_MINUTE}"
 bashio::log.info "  Log Level: ${LOG_LEVEL}"
-bashio::log.info "  Image Quality: ${IMAGE_QUALITY}%"
-bashio::log.info "  Ingress Path: ${INGRESS_PATH}"
 
 cd /app || exit 1
 
